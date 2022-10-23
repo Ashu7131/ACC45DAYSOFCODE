720940502553ba101e1c50a3b607771d6477f49c
Ashish Singh <ashishlodhi5559@gmail.com>
2022-10-23 22:30:43 +0530

    Multiplication Table Up to 10
    Write A Program Multiplication Table Up to 10

---
+++ Day - 02/table.c
@@ -0,0 +1,12 @@
+/*Write A Program Multiplication Table Up to 10*/
+
+#include <stdio.h>
+
+void main() {
+    int i,n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++){
+        printf("%d x %d = %d\n",n,i,n*i);
+    }
+}
